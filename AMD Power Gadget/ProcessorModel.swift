584e64fb335508bdfbc074584cd004bb32e19db5
trulyspinach <haoyanqi666@gmail.com>
2024-04-08 22:17:17 +1000

    update major version

--- AMD Power Gadget/ProcessorModel.swift
+++ AMD Power Gadget/ProcessorModel.swift
@@ -55,7 +55,7 @@ class ProcessorModel {
         AMDRyzenCPUPowerManagementVersion = String(cString: Array(outputStr[0...outputStrCount-1]))
         
         let compatVers = ["0.6.3", "0.6.4", "0.6.5", "0.6.6", "0.7", "0.7.1", "0.7.2"]
-        let latestMajorVers = ["0.7.1"]
+        let latestMajorVers = ["0.7.2"]
         
         if !compatVers.contains(AMDRyzenCPUPowerManagementVersion){
             alertAndQuit(message: "Your AMDRyzenCPUPowerManagement version is outdated and no longer API compatible. Please use the latest version and start this application again.")
